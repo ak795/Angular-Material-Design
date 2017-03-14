8be1517e665931956c1d5fd9e4a13a5cf45cc2f1
Anish Kumar <anishk795@gmail.com>
2017-03-14 15:22:28 +0530

    Update Functionalities
    1. Added SortBy in Ascending and Descending Order.
    2. Added Search
    3. Fixed the tool-bar.

--- app.js
+++ app.js
@@ -1,30 +1,57 @@
 var app = angular.module('WebApp', ['ngMaterial'])
 app.controller('fetchData', function($scope,$http,$rootScope) {
     $http.get('http://starlord.hackerearth.com/edfora/hackernews') .then(function(response) {
+      $scope.sortReverse = true ;
       $scope.results = response.data ;
-      $scope.$on('Reverse' , function(event,data) {
+      $scope.status = ' ' ;
+      //Sort using broadcast and 'on' on click of a button
+      $scope.$on('Type', function(event,data){
         $scope.sortType = data ;
-      });
-      $scope.$on('Type' , function(event,data){
+       }) ;
+       $scope.$on('Reverse', function(event,data){
         $scope.sortReverse = data ;
-      });
-
-    }, function(response) {
-      $scope.results = "something went wrong" ;
-      console.log($scope.results) ;
-  }) 
+       }) ;
+       $scope.$on('KeyWord', function(event,data){
+        $scope.searchKeyword = data ;
+       }) ;
+      }, function(response) {
+        $scope.results = "something went wrong" ;
+        console.log($scope.results) ;
+    }) 
 });
 
 app.config(function($mdThemingProvider) {
   $mdThemingProvider.theme('altTheme').backgroundPalette('grey').dark();
+  $mdThemingProvider.theme('input').primaryPalette('blue').accentPalette('pink').dark() ;
 });
 
-app.controller('SortController' , function($scope,$rootScope) {
-  $scope.send = function(data) {  
-    $scope.sortType = 'num_points' ;
-    $scope.sortReverse = data ;
+app.controller('BtnController' , function($scope,$rootScope,$mdDialog) {
+  $scope.sortBy = function(sortType) {  
+    $scope.sortType = sortType ;
+    $scope.sortReverse = !($scope.sortReverse) ;
 
     $rootScope.$broadcast('Reverse' , $scope.sortReverse) ;
     $rootScope.$broadcast('Type' , $scope.sortType) ;
   };
+
+  $scope.showPrompt = function(ev) {
+    var confirm = $mdDialog.prompt()
+    .title('What are you looking for?')
+    .placeholder('Search Here')
+    .ariaLabel('Tommy')
+    .targetEvent(ev)
+    .ok('Go')
+    .cancel('Cancel') ;
+
+    $mdDialog.show(confirm).then(function(result) {
+      console.log(result) ;
+      $rootScope.$broadcast('KeyWord', result) ;
+    }, function() {
+      $scope.status = 'Irrelevant results'
+    }) ;
+
+
+
+  } ;
+
 }) ;
